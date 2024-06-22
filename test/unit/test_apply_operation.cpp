3544aadf603bb0dc1e3ac6426691125dc48ff8f5
rask24 <rask0x18@gmail.com>
2024-06-22 23:29:54 +0900

    add test apply_operation

---
+++ test/unit/test_apply_operation.cpp
@@ -0,0 +1,63 @@
+// Copyright 2024, reasuke
+
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "../../src/data/data_internal.h"
+#include "data.h"
+}
+
+TEST(apply_operation, translate_x) {
+  t_data *data = (t_data *)malloc(sizeof(t_data));
+  t_point **points = (t_point **)malloc(sizeof(t_point *) * 3);
+  points[0] = (t_point *)malloc(sizeof(t_point) * 3);
+  points[1] = (t_point *)malloc(sizeof(t_point) * 3);
+  points[2] = (t_point *)malloc(sizeof(t_point) * 3);
+
+  points[0][0] = (t_point){0, 2, 1, DEFAULT_COLOR_FLAG};
+  points[0][1] = (t_point){1, 2, 2, DEFAULT_COLOR_FLAG};
+  points[0][2] = (t_point){2, 2, 3, DEFAULT_COLOR_FLAG};
+  points[1][0] = (t_point){0, 1, 4, DEFAULT_COLOR_FLAG};
+  points[1][1] = (t_point){1, 1, 5, DEFAULT_COLOR_FLAG};
+  points[1][2] = (t_point){2, 1, 6, DEFAULT_COLOR_FLAG};
+  points[2][0] = (t_point){0, 0, 7, DEFAULT_COLOR_FLAG};
+  points[2][1] = (t_point){1, 0, 8, DEFAULT_COLOR_FLAG};
+  points[2][2] = (t_point){2, 0, 9, DEFAULT_COLOR_FLAG};
+
+  data->cols = 3;
+  data->rows = 3;
+  data->points = points;
+
+  apply_operation(data, translate_x, 1.0);
+  apply_operation(data, translate_y, -1.0);
+
+  EXPECT_EQ(data->points[0][0].x, 1);
+  EXPECT_EQ(data->points[0][1].x, 2);
+  EXPECT_EQ(data->points[0][2].x, 3);
+  EXPECT_EQ(data->points[1][0].x, 1);
+  EXPECT_EQ(data->points[1][1].x, 2);
+  EXPECT_EQ(data->points[1][2].x, 3);
+  EXPECT_EQ(data->points[2][0].x, 1);
+  EXPECT_EQ(data->points[2][1].x, 2);
+  EXPECT_EQ(data->points[2][2].x, 3);
+
+  EXPECT_EQ(data->points[0][0].y, 1);
+  EXPECT_EQ(data->points[0][1].y, 1);
+  EXPECT_EQ(data->points[0][2].y, 1);
+  EXPECT_EQ(data->points[1][0].y, 0);
+  EXPECT_EQ(data->points[1][1].y, 0);
+  EXPECT_EQ(data->points[1][2].y, 0);
+  EXPECT_EQ(data->points[2][0].y, -1);
+  EXPECT_EQ(data->points[2][1].y, -1);
+  EXPECT_EQ(data->points[2][2].y, -1);
+
+  EXPECT_EQ(data->points[0][0].z, 1);
+  EXPECT_EQ(data->points[0][1].z, 2);
+  EXPECT_EQ(data->points[0][2].z, 3);
+  EXPECT_EQ(data->points[1][0].z, 4);
+  EXPECT_EQ(data->points[1][1].z, 5);
+  EXPECT_EQ(data->points[1][2].z, 6);
+  EXPECT_EQ(data->points[2][0].z, 7);
+  EXPECT_EQ(data->points[2][1].z, 8);
+  EXPECT_EQ(data->points[2][2].z, 9);
+}
