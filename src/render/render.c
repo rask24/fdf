72964c66fa56829533924ac961a24941cb879ac2
rask24 <rask0x18@gmail.com>
2024-06-23 01:36:42 +0900

    fix render

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 00:23:11 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/23 00:27:13 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/23 01:32:29 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	render(t_ctx *ctx)
 {
 	int	i;
 	int	j;
+	int	color;
 
 	i = 0;
 	while (i < ctx->data->rows)
@@ -25,8 +26,12 @@ void	render(t_ctx *ctx)
 		j = 0;
 		while (j < ctx->data->cols)
 		{
+			if (ctx->data->points[i][j].color == -1)
+				color = 0x00FFFFFF;
+			else
+				color = ctx->data->points[i][j].color;
 			plot_pixel(ctx, ctx->data->points[i][j].x,
-				ctx->data->points[i][j].y, 0xFFFFFF);
+				ctx->data->points[i][j].y, color);
 			j++;
 		}
 		i++;
