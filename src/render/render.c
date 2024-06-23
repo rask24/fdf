ccb6b54a4d04af33393365b7402b0136a014bb24
rask24 <rask0x18@gmail.com>
2024-06-23 23:25:50 +0900

    add instruction

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 00:23:11 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/23 21:33:48 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/23 23:39:43 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,38 @@
 #include "view_conf.h"
 
 #include "render_internal.h"
+
+static void	_render_instruction(t_ctx *ctx, int *y, int color, char *str)
+{
+	*y += 20;
+	mlx_string_put(ctx->mlx_conf->p_mlx,
+		ctx->mlx_conf->p_win, 20, *y, color, str);
+}
+
+static void	_render_instructions(t_ctx *ctx)
+{
+	int	color;
+	int	y;
+
+	color = 0xFFFFFF;
+	y = 0;
+	_render_instruction(ctx, &y, color, "Instructions:");
+	_render_instruction(ctx, &y, color, "1 - Isometric View");
+	_render_instruction(ctx, &y, color, "2 - Top View");
+	_render_instruction(ctx, &y, color, "W - Move Up");
+	_render_instruction(ctx, &y, color, "A - Move Left");
+	_render_instruction(ctx, &y, color, "S - Move Down");
+	_render_instruction(ctx, &y, color, "D - Move Right");
+	_render_instruction(ctx, &y, color, "F - Zoom In");
+	_render_instruction(ctx, &y, color, "G - Zoom Out");
+	_render_instruction(ctx, &y, color, "Q - Rotate Left (Y-axis)");
+	_render_instruction(ctx, &y, color, "E - Rotate Right (Y-axis)");
+	_render_instruction(ctx, &y, color, "R - Rotate Up (X-axis)");
+	_render_instruction(ctx, &y, color, "V - Rotate Down (X-axis)");
+	_render_instruction(ctx, &y, color, "Z - Rotate Counterclockwise (Z-axis)");
+	_render_instruction(ctx, &y, color, "C - Rotate Clockwise (Z-axis)");
+	_render_instruction(ctx, &y, color, "ESC - Exit");
+}
 
 static void	_reset_points(t_ctx *ctx)
 {
@@ -40,6 +72,7 @@ void	render(t_ctx *ctx, bool only_render, bool is_clean)
 		ft_bzero(ctx->mlx_conf->p_data,
 			WIN_WIDTH * WIN_HEIGHT * (ctx->mlx_conf->bits_per_pixel / 8));
 		render_image(ctx);
+		_render_instructions(ctx);
 		return ;
 	}
 	if (is_clean)
@@ -50,12 +83,13 @@ void	render(t_ctx *ctx, bool only_render, bool is_clean)
 		apply_operation(ctx->data, rotate_x, atan(1 / sqrt(2)));
 		render_image(ctx);
 		mlx_string_put(ctx->mlx_conf->p_mlx, ctx->mlx_conf->p_win,
-			20, 20, 0xFFFFFF, "isometric view");
+			920, 20, 0xFFFFFF, "isometric view");
 	}
 	else if (ctx->view_conf->type == TOPVIEW)
 	{
 		render_image(ctx);
 		mlx_string_put(ctx->mlx_conf->p_mlx, ctx->mlx_conf->p_win,
-			20, 20, 0xFFFFFF, "top view");
+			920, 20, 0xFFFFFF, "top view");
 	}
+	_render_instructions(ctx);
 }
