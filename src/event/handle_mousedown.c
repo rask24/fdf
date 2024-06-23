8dfab1a2f5329b5dd00f3bb34c1fdc38f04ae0aa
rask24 <rask0x18@gmail.com>
2024-06-24 02:44:57 +0900

    add feature scale by mouse

--- src/event/handle_mousedown.c
+++ src/event/handle_mousedown.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 02:29:56 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/24 02:40:04 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/24 02:43:54 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ int	handle_mousedown(int button, int x, int y, t_ctx *ctx)
 	{
 		ctx->view_conf->preset = (ctx->view_conf->preset + 1) % NUM_PRESETS;
 		init_colors(ctx->data, ctx->view_conf->preset);
+		render(ctx, true, false);
+	}
+	else if (button == MOUSE_SCROLL_UP)
+	{
+		apply_operation(ctx->data, scale, 1.1);
+		render(ctx, true, false);
+	}
+	else if (button == MOUSE_SCROLL_DOWN)
+	{
+		apply_operation(ctx->data, scale, 0.9);
 		render(ctx, true, false);
 	}
 	return (0);
