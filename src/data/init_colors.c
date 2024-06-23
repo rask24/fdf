ccf391bade53a79ef406319543207663233ff021
rask24 <rask0x18@gmail.com>
2024-06-23 18:22:59 +0900

    fix init_colors

--- src/data/init_colors.c
+++ src/data/init_colors.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 17:02:42 by reasuke           #+#    #+#             */
-/*   Updated: 2024/06/23 17:42:39 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/06/23 18:22:21 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,17 @@ void	init_colors(t_data *data)
 		j = 0;
 		while (j < data->cols)
 		{
-			if (data->points[i][j].color == DEFAULT_COLOR_FLAG)
+			if (data->points[i][j].color == DEFAULT_COLOR_FLAG
+				&& data->z_max != data->z_min)
 			{
 				ratio = (double)(data->points[i][j].z - data->z_min)
 					/ (data->z_max - data->z_min);
 				data->points[i][j].color = interpolate_color(
 						DEFAULT_BOTTOM_COLOR, DEFAULT_TOP_COLOR, ratio);
 			}
+			else if (data->points[i][j].color == DEFAULT_COLOR_FLAG
+				&& data->z_max == data->z_min)
+				data->points[i][j].color = DEFAULT_BOTTOM_COLOR;
 			j++;
 		}
 		i++;
